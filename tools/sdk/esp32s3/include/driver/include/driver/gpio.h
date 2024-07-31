6332d84fc3e52950f46755cd96cb3722fc5902ac
mverch67 <manuel.verch@gmx.de>
2024-07-31 23:10:24 +0200

    IO expander

--- tools/sdk/esp32s3/include/driver/include/driver/gpio.h
+++ tools/sdk/esp32s3/include/driver/include/driver/gpio.h
@@ -49,7 +49,7 @@ extern "C" {
 /// Check whether it is a valid GPIO number
 #define GPIO_IS_VALID_GPIO(gpio_num)        (((1ULL << (gpio_num)) & SOC_GPIO_VALID_GPIO_MASK) != 0)
 /// Check whether it can be a valid GPIO number of output mode
-#define GPIO_IS_VALID_OUTPUT_GPIO(gpio_num) (((1ULL << (gpio_num)) & SOC_GPIO_VALID_OUTPUT_GPIO_MASK) != 0)
+#define GPIO_IS_VALID_OUTPUT_GPIO(gpio_num) ((((1ULL << (gpio_num)) & SOC_GPIO_VALID_OUTPUT_GPIO_MASK) != 0) || (gpio_num & 0x40))
 /// Check whether it can be a valid digital I/O pad
 #define GPIO_IS_VALID_DIGITAL_IO_PAD(gpio_num) ((gpio_num >= 0) && \
                                                  (((1ULL << (gpio_num)) & SOC_GPIO_VALID_DIGITAL_IO_PAD_MASK) != 0))

7c03e08042992aaf44de72c2ab700f9138638f13
mverch67 <manuel.verch@gmx.de>
2025-08-01 10:02:57 +0200

    decrease log_d to log_v

--- cores/esp32/esp32-hal-i2c-ng.c
+++ cores/esp32/esp32-hal-i2c-ng.c
@@ -287,7 +287,7 @@ esp_err_t i2cWrite(uint8_t i2c_num, uint16_t address, const uint8_t *buff, size_
       goto end;
     }
 
-    log_d("i2c_master_transmit: bus=%u addr=0x%x handle=0x%08x size=%u", i2c_num, address, bus[i2c_num].dev_handles[address], size);
+    log_v("i2c_master_transmit: bus=%u addr=0x%x handle=0x%08x size=%u", i2c_num, address, bus[i2c_num].dev_handles[address], size);
     ret = i2c_master_transmit(bus[i2c_num].dev_handles[address], buff, size, timeOutMillis);
     if (ret != ESP_OK) {
       log_e("i2c_master_transmit failed: [%d] %s", ret, esp_err_to_name(ret));
@@ -333,7 +333,7 @@ esp_err_t i2cRead(uint8_t i2c_num, uint16_t address, uint8_t *buff, size_t size,
     goto end;
   }
 
-  log_d("i2c_master_receive: bus=%u addr=0x%x handle=0x%08x size=%u", i2c_num, address, bus[i2c_num].dev_handles[address], size);
+  log_v("i2c_master_receive: bus=%u addr=0x%x handle=0x%08x size=%u", i2c_num, address, bus[i2c_num].dev_handles[address], size);
   ret = i2c_master_receive(bus[i2c_num].dev_handles[address], buff, size, timeOutMillis);
   if (ret != ESP_OK) {
     log_e("i2c_master_receive failed: [%d] %s", ret, esp_err_to_name(ret));
@@ -381,7 +381,7 @@ esp_err_t i2cWriteReadNonStop(
     goto end;
   }
 
-  log_d("i2c_master_transmit_receive: bus=%u addr=0x%x handle=0x%08x write=%u read=%u", i2c_num, address, bus[i2c_num].dev_handles[address], wsize, rsize);
+  log_v("i2c_master_transmit_receive: bus=%u addr=0x%x handle=0x%08x write=%u read=%u", i2c_num, address, bus[i2c_num].dev_handles[address], wsize, rsize);
   ret = i2c_master_transmit_receive(bus[i2c_num].dev_handles[address], wbuff, wsize, rbuff, rsize, timeOutMillis);
   if (ret != ESP_OK) {
     log_e("i2c_master_transmit_receive failed: [%d] %s", ret, esp_err_to_name(ret));
